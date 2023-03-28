9eda67a73489a55076b9d227c5d978329e566929
Logger421 <wojteklotko3@gmail.com>
2023-03-28 22:11:36 +0200

    Updated simple bash script.

--- lab4/perform_tests.sh
+++ lab4/perform_tests.sh
@@ -3,11 +3,37 @@ if [ -f wynik-z-serwera.txt ]; then
     rm wynik-z-serwera.txt
 fi
 echo 'Performing tests...'
-./mini-udpcat.py 127.0.0.1 2020 < test-dane.txt >> wynik-z-serwera.txt
-diff -q wynik-z-serwera.txt test-wynik.txt
-if [ $? -eq 0 ];
-then
-	echo 'Tests finished sucessfully!'
-else
-	echo 'Tests failed'
-fi
+files=(
+    test-add.txt
+    test-subtract.txt
+    test-divide.txt
+    test-multiply.txt
+    test-overflow.txt
+    test-underflow.txt
+)
+
+results=(
+  add-wynik.txt
+  subtract-wynik.txt
+  divide-wynik.txt
+  multiply-wynik.txt
+  overflow-wynik.txt
+  underflow-wynik.txt
+)
+
+for file in "${files[@]}"; do
+  ./mini-udpcat.py 127.0.0.1 2020 < tests/"$file" > results/wynik-z-serwera.txt
+
+  postfix=$(echo "$file" | grep -oP "(?<=-).*?(?=\.)")
+
+  diff -s results/wynik-z-serwera.txt tests/"$postfix"-wynik.txt
+
+  if [ $? -eq 0 ];
+  then
+  	echo 'Test finished sucessfully!'
+  else
+  	echo 'Test failed'
+  fi
+done
+
+
