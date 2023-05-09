c1fa93401dfa470db46a9ff29a6e525df6eeba23
logger421 <wojteklotko3@gmail.com>
2023-05-09 14:44:32 +0200

    Updated web scrapping experiments.

--- lab7/src/main/java/webscrapper/WebScrapper.java
+++ lab7/src/main/java/webscrapper/WebScrapper.java
@@ -1,33 +1,72 @@
 package webscrapper;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class WebScrapper {
     // We will poke to -> "http://th.if.uj.edu.pl/"
-    private static URL url;
-    private static URLConnection conn;
-    private static InputStream inputStream;
+    private static boolean validateHeader(int status, String header) {
+        return status == 200 && header.equals("text/html");
+    }
 
-    public WebScrapper() {
+    private static StringBuffer readContent(BufferedReader br) throws IOException {
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = br.readLine()) != null) {
+            content.append(inputLine);
+        }
+        return content;
     }
 
     public static void main(String[] args) {
+        HttpURLConnection conn = null;
+        BufferedReader br = null;
+
         try {
-            Document soup = Jsoup.connect("http://th.if.uj.edu.pl/").get();
-            url = new URL("http://th.if.uj.edu.pl/");
-            conn = url.openConnection();
-            inputStream = conn.getInputStream();
-            int b;
-            while ((b = inputStream.read()) != -1) {
-                System.out.print((char) b);
+            Connection.Response response = Jsoup.connect("http://th.if.uj.edu.pl/").followRedirects(false).execute();
+            int statusCode = response.statusCode();
+            String statusMessage = response.statusMessage();
+            System.out.println(statusCode + " " + statusMessage);
+            if(statusCode == 200 && statusMessage.equals("text/html")) {
+                Document soup = response.parse();
+                String headTag = soup.title();
+                System.out.println(soup.title());
+            }
+
+            final URL url = new URL("http://th.if.uj.edu.pl/");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "text/html");
+            String contentType = conn.getHeaderField("Content-Type");
+            int status = conn.getResponseCode();
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            if (validateHeader(status, contentType)) {
+                StringBuffer content = readContent(br);
+                if (content.toString().contains("Institute of Theoretical Physics")){
+                    System.out.println("OK");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("FAILURE");
+                    System.exit(1);
+                }
+            }
+            else {
+                System.out.println("Error return status code: " + status);
+                System.exit(1);
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("Clearing");
+            if (conn != null) conn.disconnect();
         }
     }
 }
